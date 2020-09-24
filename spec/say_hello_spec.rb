f7ab0a980af132ca43f353dcfd149adf15c59650
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-24 01:05:02 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 
-describe "say_hello" do 
-  
+describe "say_hello" do
+
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
@@ -14,7 +14,7 @@ describe "say_hello" do
 end
 
 def say_hello(name ="ruby the programmer")
-  puts "Hello #{name} !" 
+  puts "Hello #{name} !"
 end
 
 say_hello("Gabriela")
